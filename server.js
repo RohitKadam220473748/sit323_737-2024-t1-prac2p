afdfe68a9d49a9009f7a4a140246a7dbea2c2e5a
ROHIT KADAM <rohitkadam680@gmail.com>
2024-03-12 21:15:43 +1100

    done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require("express")
-var app = express()
-var port = process.env.port || 3000;
-app.listen(port,()=>{
+var express = require("express")                     //  this line imports Express.js library 
+var app = express()                                  // this line creates an instance for express application
+var port = process.env.port || 3000;                 // it specify the port number for the server 
+app.listen(port,()=>{                                 // it starts the express server on the specified port and logs a message indicating that App listening to the specified port.
 console.log("App listening to: "+port)
 })
--- server_get.js
+++ server_get.js
@@ -1,17 +1,17 @@
-const express= require("express");
-    const res = require("express/lib/response");
-    const app= express();
-    const addTwoNumber= (n1,n2) => {
-        return n1+n2;
-    }
-    app.get("/addTwoNumber", (req,res)=>{
-        const n1= parseInt(req.query.n1);
-        const n2=parseInt(req.query.n2);
-        const result = addTwoNumber(n1,n2);
-        res.json({statuscocde:200, data: result }); 
-    });
-    console.log (addTwoNumber(19,12));
-    const port=3040;
-    app.listen(port,()=> {
-        console.log("hello i'm listening to port "+port);
-    })
+const express = require("express");                      //  this line imports Express.js library
+const res = require("express/lib/response");        // this line imports the response object from the Express library 
+const app = express();                               // this line creates an instance for express application
+const addTwoNumber = (n1, n2) => {                    // it creates a function to add two number which takes n1 and n2 and returns their sum
+    return n1 + n2;
+}
+app.get("/addTwoNumber", (req, res) => {               // it Define a route to handle GET requests at the '/addTwoNumber' endpoint 
+    const n1 = parseInt(req.query.n1);                // it parses the query parameters n1 and n2 to integers from the request URL 
+    const n2 = parseInt(req.query.n2);
+    const result = addTwoNumber(n1, n2);               // it calls the addTwoNumber function to add the two numbers and stores it in result
+    res.json({ statuscode: 200, data: result });      // it sends a JSON response with the sum of the two numbers
+});
+console.log(addTwoNumber(19, 12));                     // it logs the result of adding two numbers 
+const port = 3040;                                      // it specify the port number for the server 
+app.listen(port, () => {                                  // it starts the express server on the specified port and logs a message indicating that it's listening on the specified port.
+    console.log("hello i'm listening to port " + port);
+})
